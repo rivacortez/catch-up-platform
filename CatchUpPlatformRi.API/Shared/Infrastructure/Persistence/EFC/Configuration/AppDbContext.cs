9720bded4401fe86483c9461b295b961641d1206
rivacortezf <u202215313@upc.edu.pe>
2024-07-07 18:20:03 -0500

    feat:implement AppDbContext

--- CatchUpPlatformRi.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ CatchUpPlatformRi.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,6 +1,25 @@
-﻿namespace CatchUpPlatformRi.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+﻿using CatchUpPlatformRi.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using Microsoft.EntityFrameworkCore;
 
-public class AppDbContext
+namespace CatchUpPlatformRi.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+public class AppDbContext: DbContext
 {
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        builder.AddCreatedUpdatedInterceptor();
+        base.OnConfiguring(builder);
+    }
     
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        builder.UseSnakeCaseNamingConvention();
+    }
 }
