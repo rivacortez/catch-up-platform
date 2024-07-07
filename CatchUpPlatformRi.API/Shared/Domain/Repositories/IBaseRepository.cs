9952323fbd72af7b99d2d7db03ba9a577598e13d
rivacortezf <u202215313@upc.edu.pe>
2024-07-07 16:57:19 -0500

    feat:implement IBaseRepository and IUnitOfWork (Repository Patern)

--- CatchUpPlatformRi.API/Shared/Domain/Repositories/IBaseRepository.cs
+++ CatchUpPlatformRi.API/Shared/Domain/Repositories/IBaseRepository.cs
@@ -1,6 +1,11 @@
 ﻿namespace CatchUpPlatformRi.API.Shared.Domain.Repositories;
 
-public interface IBaseRepository
+public interface IBaseRepository<TEntity>
 {
-    
+    Task AddAsync(TEntity entity);
+    Task<TEntity?> FindByAsync(int id);
+
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+
 }
