3227f7dd59ea0dd4b23a3d8653d47c5fa5774914
rivacortezf <u202215313@upc.edu.pe>
2024-07-07 17:33:08 -0500

    feat:implement interfaces configuration

--- CatchUpPlatformRi.API/Shared/Infrastructure/Interfaces/ASP/Configuration/Extensions/StringExtensions.cs
+++ CatchUpPlatformRi.API/Shared/Infrastructure/Interfaces/ASP/Configuration/Extensions/StringExtensions.cs
@@ -1,6 +1,23 @@
-﻿namespace CatchUpPlatformRi.API.Shared.Infrastructure.Interfaces.ASP.Configuration.Extensions;
+﻿using System.Text.RegularExpressions;
 
-public class StringExtensions
+namespace CatchUpPlatformRi.API.Shared.Infrastructure.Interfaces.ASP.Configuration.Extensions;
+
+public static partial class StringExtensions
 {
+
+    public static string ToKebabCase(this string text)
+    {
+        if (string.IsNullOrEmpty(text))
+        {
+            return text;
+        }
+
+        return KebabCaseRegex().Replace(text, "-$1")
+            .Trim()
+            .ToLower();
+
+    }
+     [GeneratedRegex("(?<!^)([A-Z][a-z]|(?<=[a-z])[A-Z])",RegexOptions.Compiled)]
+     private static partial Regex KebabCaseRegex();
     
 }
--- CatchUpPlatformRi.API/Shared/Infrastructure/Interfaces/ASP/Configuration/KebabCaseRouteNamingConvention.cs
+++ CatchUpPlatformRi.API/Shared/Infrastructure/Interfaces/ASP/Configuration/KebabCaseRouteNamingConvention.cs
@@ -1,6 +1,30 @@
-﻿namespace CatchUpPlatformRi.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using  CatchUpPlatformRi.API.Shared.Infrastructure.Interfaces.ASP.Configuration.Extensions;
+using Microsoft.AspNetCore.Mvc;
 
-public class KebabCaseRouteNamingConvention
+namespace CatchUpPlatformRi.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
+
+public class KebabCaseRouteNamingConvention: IControllerModelConvention
 {
+    private static AttributeRouteModel? ReplaceControllerTemplate(SelectorModel selector, string name)
+    {
+        return selector.AttributeRouteModel != null ? new AttributeRouteModel
+        {
+            Template = selector.AttributeRouteModel.Template?.Replace("[controller]", name.ToKebabCase())
+        } : null;
+    }
+
+    public void Apply(ControllerModel controller)
+    {
+        foreach (var selector in controller.Selectors)
+        {
+            selector.AttributeRouteModel = ReplaceControllerTemplate(selector, controller.ControllerName);
+        }
+
+        foreach (var selector in controller.Actions.SelectMany(a=>a.Selectors))
+        {
+            selector.AttributeRouteModel = ReplaceControllerTemplate(selector, controller.ControllerName);
+        }
+    }
     
 }
