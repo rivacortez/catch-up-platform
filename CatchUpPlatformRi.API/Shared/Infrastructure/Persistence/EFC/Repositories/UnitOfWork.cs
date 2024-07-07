ca082bdbb911c81dadb30d7036db4eaaaf9a7b20
rivacortezf <u202215313@upc.edu.pe>
2024-07-07 18:39:42 -0500

    fead:implemented UnitOfWork(Persistence)

--- CatchUpPlatformRi.API/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
+++ CatchUpPlatformRi.API/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -1,6 +1,19 @@
-﻿namespace CatchUpPlatformRi.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+﻿using CatchUpPlatformRi.API.Shared.Domain.Repositories;
+using CatchUpPlatformRi.API.Shared.Infrastructure.Persistence.EFC.Configuration;
 
-public class UnitOfWork
+namespace CatchUpPlatformRi.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+public class UnitOfWork:IUnitOfWork
 {
-    
+    private readonly AppDbContext _context;
+
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task CompleteAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
 }
