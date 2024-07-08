7de20468664545b11799d9f838e6342adb25632b
rivacortezf <u202215313@upc.edu.pe>
2024-07-07 23:54:50 -0500

    fix: 6  searching error code (BUG)

--- CatchUpPlatformRi.API/News/Interfaces/REST/Transform/FavoriteSourceResourceFromEntityAssembler.cs
+++ CatchUpPlatformRi.API/News/Interfaces/REST/Transform/FavoriteSourceResourceFromEntityAssembler.cs
@@ -7,5 +7,5 @@ namespace CatchUpPlatformRi.API.News.Interfaces.REST.Transform;
 public static class FavoriteSourceResourceFromEntityAssembler
 {
     public static FavoriteSourceResource ToResourceFromEntity(FavoriteSource entity) =>
-        new FavoriteSourceResource(entity.Id,entity.NewsApiKey,entity.SourceId);
+        new FavoriteSourceResource(entity.Id, entity.NewsApiKey, entity.SourceId);
 }
