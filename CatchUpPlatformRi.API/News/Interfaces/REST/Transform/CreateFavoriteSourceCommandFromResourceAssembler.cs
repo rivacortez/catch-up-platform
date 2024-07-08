66d17bc0dc5db0e95e8d18aaae36a48cf4bd5710
rivacortezf <u202215313@upc.edu.pe>
2024-07-07 22:38:18 -0500

    feat:added Transform (Interfaces) on News Bounded Context

---
+++ CatchUpPlatformRi.API/News/Interfaces/REST/Transform/CreateFavoriteSourceCommandFromResourceAssembler.cs
@@ -0,0 +1,11 @@
+﻿using CatchUpPlatformRi.API.News.Domain.Model.Commands;
+using CatchUpPlatformRi.API.News.Interfaces.REST.Resources;
+
+namespace CatchUpPlatformRi.API.News.Interfaces.REST.Transform;
+
+public static class CreateFavoriteSourceCommandFromResourceAssembler
+{
+    public static CreateFavoriteSourceCommand ToCommandFromResource(CreateFavoriteSourceResource resource) =>
+        new CreateFavoriteSourceCommand(resource.NewsApiKey, resource.SourceId);
+  
+}
---
+++ CatchUpPlatformRi.API/News/Interfaces/REST/Transform/FavoriteSourceResourceFromEntityAssembler.cs
@@ -0,0 +1,11 @@
+﻿using CatchUpPlatformRi.API.News.Domain.Model.Aggregates;
+using CatchUpPlatformRi.API.News.Domain.Model.Commands;
+using CatchUpPlatformRi.API.News.Interfaces.REST.Resources;
+
+namespace CatchUpPlatformRi.API.News.Interfaces.REST.Transform;
+
+public static class FavoriteSourceResourceFromEntityAssembler
+{
+    public static FavoriteSourceResource TorResourceFromEntity(FavoriteSource entity) =>
+        new FavoriteSourceResource(entity.Id,entity.NewsApiKey,entity.SourceId);
+}
