db95d13c06f54d1a516a9ddce8a658593e9a73ae
rivacortezf <u202215313@upc.edu.pe>
2024-07-07 23:55:47 -0500

    fix: 7  searching error code (BUG)

--- CatchUpPlatformRi.API/News/Interfaces/REST/FavoriteSourcesController.cs
+++ CatchUpPlatformRi.API/News/Interfaces/REST/FavoriteSourcesController.cs
@@ -7,36 +7,31 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CatchUpPlatformRi.API.News.Interfaces.REST;
 
-
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/v1/[controller]")]
 [Produces(MediaTypeNames.Application.Json)]
-
-
 public class FavoriteSourcesController(
     IFavoriteSourceCommandService favoriteSourceCommandService,
     IFavoriteSourceQueryService favoriteSourceQueryService)
-    :ControllerBase
-
+    : ControllerBase
 {
     [HttpPost]
-    public async Task<ActionResult> CreateFavoriteSource(
-        [FromBody] CreateFavoriteSourceResource resource)
+    public async Task<ActionResult> CreateFavoriteSource([FromBody] CreateFavoriteSourceResource resource)
     {
         var createFavoriteSourceCommand = CreateFavoriteSourceCommandFromResourceAssembler.ToCommandFromResource(resource);
         var result = await favoriteSourceCommandService.Handle(createFavoriteSourceCommand);
         if (result is null) return BadRequest();
         return CreatedAtAction(nameof(GetFavoriteSourceById), new { id = result.Id }, FavoriteSourceResourceFromEntityAssembler.ToResourceFromEntity(result));
     }
-
+    
     private async Task<ActionResult> GetAllFavoriteSourcesByNewsApiKey(string newsApiKey)
     {
-        var getAllFavoriteSourcesByNewsApiKeyQuery = new GetAllFavoriteSourcesByNewsApiKeyQuery(newsApiKey);
+       var getAllFavoriteSourcesByNewsApiKeyQuery = new GetAllFavoriteSourcesByNewsApiKeyQuery(newsApiKey);
         var result = await favoriteSourceQueryService.Handle(getAllFavoriteSourcesByNewsApiKeyQuery);
         var resources = result.Select(FavoriteSourceResourceFromEntityAssembler.ToResourceFromEntity);
         return Ok(resources);
     }
-
+    
     private async Task<ActionResult> GetFavoriteSourceByNewsApiKeyAndSourceId(string newsApiKey, string sourceId)
     {
         var getFavoriteSourceByNewsApiKeyAndSourceIdQuery = new GetFavoriteSourceByNewsApiKeyAndSourceIdQuery(newsApiKey, sourceId);
@@ -45,7 +40,6 @@ public class FavoriteSourcesController(
         var resource = FavoriteSourceResourceFromEntityAssembler.ToResourceFromEntity(result);
         return Ok(resource);
     }
-    
     [HttpGet]
     public async Task<ActionResult> GetFavoriteSourceFromQuery([FromQuery] string newsApiKey, [FromQuery] string sourceId = "")
     {
@@ -53,7 +47,7 @@ public class FavoriteSourcesController(
             ? await GetAllFavoriteSourcesByNewsApiKey(newsApiKey)
             : await GetFavoriteSourceByNewsApiKeyAndSourceId(newsApiKey, sourceId);
     }
-
+    
     [HttpGet("{id}")]
     public async Task<ActionResult> GetFavoriteSourceById(int id)
     {
