6b590b60a29e0c3cd2a70573d026e19a212f561e
rivacortezf <u202215313@upc.edu.pe>
2024-07-07 20:39:19 -0500

    feat: added IFavoriteSourceRepository (News)

---
+++ CatchUpPlatformRi.API/News/Domain/Repositories/IFavoriteSourceRepository.cs
@@ -0,0 +1,10 @@
+﻿using CatchUpPlatformRi.API.News.Domain.Model.Aggregates;
+using CatchUpPlatformRi.API.Shared.Domain.Repositories;
+
+namespace CatchUpPlatformRi.API.News.Domain.Repositories;
+
+public interface IFavoriteSourceRepository: IBaseRepository<FavoriteSource>
+{
+    Task<IEnumerable<FavoriteSource>> FindByNewsApiKeyAsync(string newsApiKey);
+    Task<FavoriteSource?> FindByNewsApiKeyAndSourceIdAsync(string newsApiKey, string sourceId);
+}
