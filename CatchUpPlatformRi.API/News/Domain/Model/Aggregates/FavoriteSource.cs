6ef6930e29f4d109eb8bedc3f7d6b738a5b927c0
rivacortezf <u202215313@upc.edu.pe>
2024-07-07 23:44:48 -0500

    fix: 2  searching error code (BUG)

--- CatchUpPlatformRi.API/News/Domain/Model/Aggregates/FavoriteSource.cs
+++ CatchUpPlatformRi.API/News/Domain/Model/Aggregates/FavoriteSource.cs
@@ -2,22 +2,34 @@
 
 namespace CatchUpPlatformRi.API.News.Domain.Model.Aggregates;
 
+/// FavoriteSource Aggregate
+/// <summary>
+/// This class represents the FavoriteSource aggregate. It is used to store the favorite sources of a user.
+/// </summary>
+/// 
 public class FavoriteSource
 {
     public int Id { get; private set; }
     public string NewsApiKey { get; private set; }
     public string SourceId { get; private set; }
-
+    
     protected FavoriteSource()
     {
         this.NewsApiKey = string.Empty;
         this.SourceId = string.Empty;
     }
 
+    /// <summary>
+    /// Constructor for the FavoriteSource aggregate.
+    /// </summary>
+    /// <remarks>
+    /// The constructor is the command handler for the CreateFavoriteSourceCommand.
+    /// </remarks>
+    /// <param name="command">The CreateFavoriteSourceCommand command</param>
+    /// 
     public FavoriteSource(CreateFavoriteSourceCommand command)
     {
         this.NewsApiKey = command.NewsApiKey;
         this.SourceId = command.SourceId;
     }
-    
 }
