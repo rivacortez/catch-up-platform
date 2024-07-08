064b36a4e7e931248b1bdc11728d74fabc40fa34
rivacortezf <u202215313@upc.edu.pe>
2024-07-07 20:47:41 -0500

    feat:implemented  Services IFavoriteSource (Command & Queries)

--- CatchUpPlatformRi.API/News/Domain/Services/IFavoriteSourceCommandService.cs
+++ CatchUpPlatformRi.API/News/Domain/Services/IFavoriteSourceCommandService.cs
@@ -1,6 +1,9 @@
-﻿namespace CatchUpPlatformRi.API.News.Domain.Services;
+﻿using CatchUpPlatformRi.API.News.Domain.Model.Aggregates;
+using CatchUpPlatformRi.API.News.Domain.Model.Commands;
+
+namespace CatchUpPlatformRi.API.News.Domain.Services;
 
 public interface IFavoriteSourceCommandService
 {
-    
+    Task<FavoriteSource?> Handle(CreateFavoriteSourceCommand command);
 }
--- CatchUpPlatformRi.API/News/Domain/Services/IFavoriteSourceQueryService.cs
+++ CatchUpPlatformRi.API/News/Domain/Services/IFavoriteSourceQueryService.cs
@@ -1,6 +1,11 @@
-﻿namespace CatchUpPlatformRi.API.News.Domain.Services;
+﻿using CatchUpPlatformRi.API.News.Domain.Model.Aggregates;
+using CatchUpPlatformRi.API.News.Domain.Model.Queries;
+
+namespace CatchUpPlatformRi.API.News.Domain.Services;
 
 public interface IFavoriteSourceQueryService
 {
-    
+    Task<IEnumerable<FavoriteSource>> Handle(GetAllFavoriteSourcesByNewsApiKeyQuery query);
+    Task<FavoriteSource?> Handle(GetFavoriteSourceByNewsApiKeyAndSourceIdQuery query);
+    Task<FavoriteSource?> Handle(GetFavoriteSourceByIdQuery query);
 }
