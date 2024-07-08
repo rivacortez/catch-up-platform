5fc2882b433ee013fceb0abc51b6feccaecced7f
rivacortezf <u202215313@upc.edu.pe>
2024-07-07 22:13:54 -0500

    feat: added FavoriteSourceQueryService of Internal Application (News) Bounded Context

---
+++ CatchUpPlatformRi.API/News/Application/Internal/QueryServices/FavoriteSourceQueryService.cs
@@ -0,0 +1,25 @@
+﻿using CatchUpPlatformRi.API.News.Domain.Model.Aggregates;
+using CatchUpPlatformRi.API.News.Domain.Model.Queries;
+using CatchUpPlatformRi.API.News.Domain.Repositories;
+using CatchUpPlatformRi.API.News.Domain.Services;
+
+namespace CatchUpPlatformRi.API.News.Application.Internal.QueryServices;
+
+public class FavoriteSourceQueryService(IFavoriteSourceRepository favoriteSourceRepository)
+             : IFavoriteSourceQueryService
+{
+    public  async Task<IEnumerable<FavoriteSource>> Handle(GetAllFavoriteSourcesByNewsApiKeyQuery query)
+    {
+        return await favoriteSourceRepository.FindByNewsApiKeyAsync(query.NewsApiKey);
+    }
+
+    public async Task<FavoriteSource?> Handle(GetFavoriteSourceByNewsApiKeyAndSourceIdQuery query)
+    {
+        return await favoriteSourceRepository.FindByNewsApiKeyAndSourceIdAsync(query.NewsApiKey,query.SourceId);
+    }
+
+    public async Task<FavoriteSource?> Handle(GetFavoriteSourceByIdQuery query)
+    {
+        return await favoriteSourceRepository.FindByIdAsync(query.Id);
+    }
+}
