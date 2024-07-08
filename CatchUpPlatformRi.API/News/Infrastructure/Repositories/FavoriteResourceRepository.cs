88aa9121ade5012332adfcb49bd3a4d3702e3dad
rivacortezf <u202215313@upc.edu.pe>
2024-07-07 21:00:23 -0500

    feat: Implemented FavoriteResourceRepository (Infrastructure)

--- CatchUpPlatformRi.API/News/Infrastructure/Repositories/FavoriteResourceRepository.cs
+++ CatchUpPlatformRi.API/News/Infrastructure/Repositories/FavoriteResourceRepository.cs
@@ -1,6 +1,25 @@
-﻿namespace CatchUpPlatformRi.API.News.Infrastructure.Repositories;
+﻿using CatchUpPlatformRi.API.News.Domain.Model.Aggregates;
+using CatchUpPlatformRi.API.News.Domain.Repositories;
+using CatchUpPlatformRi.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using CatchUpPlatformRi.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
 
-public class FavoriteResourceRepository
+namespace CatchUpPlatformRi.API.News.Infrastructure.Repositories;
+
+public class FavoriteSourceRepository : BaseRepository<FavoriteSource>, IFavoriteSourceRepository
 {
-    
+    public FavoriteSourceRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<FavoriteSource>> FindByNewsApiKeyAsync(string newsApiKey)
+    {
+        return await Context.Set<FavoriteSource>().Where(f => f.NewsApiKey == newsApiKey).ToListAsync();
+    }
+
+    public async Task<FavoriteSource?> FindByNewsApiKeyAndSourceIdAsync(string newsApiKey, string sourceId)
+    {
+       return await Context.Set<FavoriteSource>().FirstOrDefaultAsync(f=>f.NewsApiKey== newsApiKey && f.SourceId == sourceId);
+       
+    }
 }
