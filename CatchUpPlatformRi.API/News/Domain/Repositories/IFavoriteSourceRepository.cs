6c6b68e43ffa14760b356ca4609caef3194d0c86
rivacortezf <u202215313@upc.edu.pe>
2024-07-07 23:47:55 -0500

    fix: 4  searching error code (BUG)

--- CatchUpPlatformRi.API/News/Domain/Repositories/IFavoriteSourceRepository.cs
+++ CatchUpPlatformRi.API/News/Domain/Repositories/IFavoriteSourceRepository.cs
@@ -3,7 +3,7 @@ using CatchUpPlatformRi.API.Shared.Domain.Repositories;
 
 namespace CatchUpPlatformRi.API.News.Domain.Repositories;
 
-public interface IFavoriteSourceRepository: IBaseRepository<FavoriteSource>
+public interface IFavoriteSourceRepository : IBaseRepository<FavoriteSource>
 {
     Task<IEnumerable<FavoriteSource>> FindByNewsApiKeyAsync(string newsApiKey);
     Task<FavoriteSource?> FindByNewsApiKeyAndSourceIdAsync(string newsApiKey, string sourceId);
