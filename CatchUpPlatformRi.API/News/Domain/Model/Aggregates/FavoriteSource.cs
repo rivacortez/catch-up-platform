13aac5baf6a56bb62776e5188868e6fd7f351ad6
rivacortezf <u202215313@upc.edu.pe>
2024-07-07 20:18:26 -0500

    feat:implemented FavoriteSource Aggregate Model (News)

--- CatchUpPlatformRi.API/News/Domain/Model/Aggregates/FavoriteSource.cs
+++ CatchUpPlatformRi.API/News/Domain/Model/Aggregates/FavoriteSource.cs
@@ -2,5 +2,15 @@
 
 public class FavoriteSource
 {
+    public int Id { get; private set; }
+    public string NewsApiKey { get; private set; }
+    public string SourceId { get; private set; }
+
+    protected FavoriteSource()
+    {
+        this.NewsApiKey = string.Empty;
+        this.SourceId = string.Empty;
+    }
+    
     
 }
